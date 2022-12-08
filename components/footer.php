77cf4c9a6a7db88bc8a88616788dd5b860d75e0b
Doan Quang Huy <2015597@dlu.edu.vn>
2022-12-08 14:00:29 +0700

    upload project

---
+++ components/footer.php
@@ -0,0 +1,40 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="imgs/email-icon.png" alt="">
+         <h3>email liên hệ</h3>
+         <a href="mailto:quanghuybest@gmail.com">quanghuybest@gmail.com</a>
+         <a href="mailto:suppersun1177@gmail.com">suppersun1177@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="imgs/clock-icon.png" alt="">
+         <h3>giờ mở cửa</h3>
+         <p>Sáng: 07:00 đến 11:00</p>
+         <p>Chiều: 13:00 đến 18:00</p>
+      </div>
+
+      <div class="box">
+         <img src="imgs/map-icon.png" alt="">
+         <h3>địa chỉ</h3>
+         <a href="#">17 Nguyễn Công Trứ, Phường 8, TP Đà Lạt, Lâm Đồng.</a>
+      </div>
+
+      <div class="box">
+         <img src="imgs/phone-icon.png" alt="">
+         <h3>số điện thoại</h3>
+         <a href="tel://0859476166">0859476166</a>
+         <a href="tel://0123456789">0123456789</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Đoàn Quang Huy</span> | all rights reserved!</div>
+
+</footer>
+
+<div class="loader">
+   <img src="imgs/loading.gif" alt="">
+</div>
